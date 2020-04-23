24c5469a4e2baa0416cb891f74aa4e4b61e21e3e
a18nadma <49141885+a18nadma@users.noreply.github.com>
2020-04-23 13:55:21 +0200

    Added onclick

--- app/src/main/java/com/example/screens/MainActivity.java
+++ app/src/main/java/com/example/screens/MainActivity.java
@@ -4,12 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    Intent intent = new Intent(MainActivity.this, Smile.class);
-    startActivity(intent);
+   Button button = (Button)findViewById(button);
+   button.setOnClickListener(new View.OnClickListener()
 
+    {
+        @Override
+        public void onClick(View v){
+        startActivity(new Intent(MainActivity.this, Smile.class));
+    }
+    });
 
 
     @Override
